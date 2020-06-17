80548b9091ba0b74b16c1ff1ecade5b33ed36683
s-ari <sgt.arima@gmail.com>
2020-06-17 15:47:43 +0900

    edit upload process

--- Dockerfile/php-apache/app/upload.php
+++ Dockerfile/php-apache/app/upload.php
@@ -3,34 +3,28 @@
 <meta charset=UFT-8>
 <title>Ari Image Viewer</title>
 <style>
-    body {
-      background-image: linear-gradient(-225deg, #2CD8D5 0%, #C5C1FF 56%, #FFBAC3 100%);;
-    }
+body {
+  background-image: linear-gradient(-225deg, #2CD8D5 0%, #C5C1FF 56%, #FFBAC3 100%);;
+}
 </style>
 </head>
 <body>
-<body>
-<form method="post" enctype="multipart/form-data">
-  <input type="file" name="img"><br><br>
-  <input type="text" name="img_name"> file name<br><br>
-  <input type="submit" value="register">
-</form>
     <?php
         // DB認証情報取得 using GetAuth or GetAuthEnv
         require_once("get_auth_function.php");
         //GetAuth();
         GetAuthEnv();
-       
+     
         // DB認証情報設定
         $dsn  = "mysql:dbname=$dbname;host=$host;charset=utf8";
         $driver_options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
-      
+     
         // DB接続
         $pdo = new PDO($dsn, $user, $pw, $driver_options);
-        
+     
         // SQL
         $sql = '
               INSERT INTO
@@ -38,10 +32,10 @@
               VALUES
                   (:img_name, :img)
         ';
-      
+     
         // 画像データ取得
         $img_data = file_get_contents($_FILES['img']['tmp_name']);
-      
+     
         // 画像保存
         $stmt = $pdo->prepare($sql);
         $stmt->bindValue(':img_name', $_POST['img_name'], PDO::PARAM_STR);
@@ -50,3 +44,4 @@
     ?>
 </body>
 </html>
+
